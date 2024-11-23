fea201599526a88009a803413823f78641ef9639
ScandalousPigeon <951413eelnoel@gmail.com>
2024-11-24 02:29:35 +1000

    added draw_move method which draws line from middle of one cell to another

--- drawing.py
+++ drawing.py
@@ -22,25 +22,47 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
 
-        self._top_left = Point(x1, y1)
-        self._top_right = Point(x2, y1)
-        self._bottom_left = Point(x1, y2)
-        self._bottom_right = Point(x2, y2)
+        
 
         self._win = win
 
     def draw(self):
+        top_left = Point(self._x1, self._y1)
+        top_right = Point(self._x2, self._y1)
+        bottom_left = Point(self._x1, self._y2)
+        bottom_right = Point(self._x2, self._y2)
         if self.has_left_wall:
-            left_wall = Line(self._top_left, self._bottom_left)
+            left_wall = Line(top_left, bottom_left)
             self._win.draw_line(left_wall, "black")
         if self.has_right_wall:
-            right_wall = Line(self._top_right, self._bottom_right)
+            right_wall = Line(top_right, bottom_right)
             self._win.draw_line(right_wall, "black")
         if self.has_top_wall:
-            top_wall = Line(self._top_left, self._top_right)
+            top_wall = Line(top_left, top_right)
             self._win.draw_line(top_wall, "black")
         if self.has_bottom_wall:
-            bottom_wall = Line(self._bottom_left, self._bottom_right)
+            bottom_wall = Line(bottom_left, bottom_right)
             self._win.draw_line(bottom_wall, "black")
             
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            colour = "gray"
+        else:
+            colour = "red"
+
+        own_centre_x = (self._x1 + self._x2) / 2
+        own_centre_y = (self._y1 + self._y2) / 2
+        own_centre = Point(own_centre_x, own_centre_y)
+
+        other_centre_x = (to_cell._x1 + to_cell._x2) / 2
+        other_centre_y = (to_cell._y1 + to_cell._y2) / 2
+        other_centre = Point(other_centre_x, other_centre_y)
+
+        new_line = Line(own_centre, other_centre)
+
+        self._win.draw_line(new_line, colour)
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def main():
     window = Window(800, 600)
     
     dead_cells = Cell(50, 50, 110, 110, window)
+    second_cell = Cell(150, 150, 400, 400, window)
     dead_cells.draw()
+    second_cell.draw()
+    dead_cells.draw_move(second_cell, True)
     window.wait_for_close()
 
 if __name__ == "__main__":
